ce77072a4c5b7ecd02918c80159629ebd4c3e615
xuanfeiyu <474266267@qq.com>
2021-01-30 02:52:23 +0800

    feat: 练习

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ff88def23c9d5128437f483495d50c67'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '572aa7d3-948a-420a-b853-d5721848ffad'
+};
